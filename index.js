ef2c6e5ca979d6180355c9ede2cf869220aa7444
Oscar Valencia <o.valencia@polamer.us>
2018-02-21 11:06:06 -0500

    beta version with languges

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = function() {
-  console.log('Hello Mars!!!!!!!!!')
+module.exports = function(lang) {
+	if(lang ==="es")
+		console.log('Hola Marte!!')
+}   
+
+	if(lang === "it"){
+		console.log('Ciao Marte!!')
+}	
+
+else {
+		console.log('Hello Mars!!!!!!!!!')
+}
+
 }
